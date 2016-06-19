2e252273182795b134469caf0a4f901a87ceca49
John Else <john.else@gmail.com>
2016-06-19 09:02:40 +0100

    Initial commit
    Signed-off-by: John Else <john.else@gmail.com>

---
+++ src/script.ml
@@ -0,0 +1,11 @@
+let start _ =
+  let start_button = Dom_html.getElementById("start") in
+  start_button##onclick <- Dom_html.handler (fun _ ->
+    let element = Dom_html.document##createElement (Js.string "button") in
+    let node = (element :> Dom.node) in
+    Dom_html.document##body##appendChild (node);
+
+  Js._false)
+
+let _ =
+  Dom_html.window##onload <- Dom_html.handler start
