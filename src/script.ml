29199839dc9c5fb787030d9425060e77bfea5c06
John Else <john.else@gmail.com>
2016-06-20 22:57:07 +0100

    Fix type coercion
    Signed-off-by: John Else <john.else@gmail.com>

--- src/script.ml
+++ src/script.ml
@@ -2,8 +2,8 @@ let start _ =
   let start_button = Dom_html.getElementById("start") in
   start_button##onclick <- Dom_html.handler (fun _ ->
     let element = Dom_html.document##createElement (Js.string "button") in
-    let node = (element :> Dom.node) in
-    Dom_html.document##body##appendChild (node);
+    let node = (element :> Dom.node Js.t) in
+    ignore (Dom_html.document##body##appendChild (node));
 
     Js._false);
 
