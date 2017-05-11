9dada2b05f01df14df385fc04b0eb23d5d83f2ec
John Else <john.else@gmail.com>
2017-05-11 10:35:40 +0100

    Convert to js_of_ocaml.ppx
    Signed-off-by: John Else <john.else@gmail.com>

--- src/script.ml
+++ src/script.ml
@@ -1,13 +1,13 @@
 let start _ =
   let start_button = Dom_html.getElementById("start") in
-  start_button##onclick <- Dom_html.handler (fun _ ->
+  start_button##.onclick := Dom_html.handler (fun _ ->
     let element = Dom_html.document##createElement (Js.string "button") in
     let node = (element :> Dom.node Js.t) in
-    ignore (Dom_html.document##body##appendChild (node));
+    ignore (Dom_html.document##.body##appendChild (node));
 
     Js._false);
 
   Js._false
 
 let _ =
-  Dom_html.window##onload <- Dom_html.handler start
+  Dom_html.window##.onload := Dom_html.handler start
